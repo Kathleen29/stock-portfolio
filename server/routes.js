0860f8408bb487492c9cfab9cfd977cfc2dce167
Kathleen Trainor <kathleentrainor9@gmail.com>
2020-02-24 17:45:14 -0500

    Adds API call to fetch stock quote from IEX

--- server/routes.js
+++ server/routes.js
@@ -6,4 +6,19 @@ router.get('/', (req, res) => {
 	res.sendStatus(200);
 });
 
+// fetches quote for a given symbol from IEX API
+router.get('/buy/:ticker/:qty', async(req, res) => {
+	try {
+		let quote = await axios.get(`https://cloud.iexapis.com/stable/stock/${req.params.ticker}/quote?token=${API_KEY}`);
+		res.send({
+			ticker: req.params.ticker,
+			shares: req.params.qty,
+			price: quote.data.latestPrice
+		});
+	}
+	catch {
+		res.send(404);
+	}
+});
+
 module.exports = router;
