0f1c21b52675a13a556cb81027e0a9e02a710349
Kathleen Trainor <kathleentrainor9@gmail.com>
2020-02-26 17:56:56 -0500

    Updates Portfolio component to render empty row if no stocks have been purchased by user

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -3,19 +3,21 @@ import Buy from './Buy.jsx';
 
 const Portfolio = (props) => {
   return (
-    <span>
+    <div>
       <h2>Portfolio $({})</h2>
       <table id='portfolio-table'></table>
-      {props.portfolio.map((stock) =>
-        <tr>
-          <td>{stock.ticker}</td>
-          <td>{stock.shares} Shares</td>
-          <td>${stock.shares * currPrice}</td>
-        </tr>
-      )}
+      { (props.portfolio)
+        ? props.portfolio.map((stock) =>
+          <tr>
+            <td>{stock.ticker}</td>
+            <td>{stock.shares} Shares</td>
+            <td>${stock.shares * currPrice}</td>
+          </tr>)
+        : <tr></tr>
+      }
     })
       <Buy userId={props.user}/>
-    </span>
+    </div>
   )
 };
 
