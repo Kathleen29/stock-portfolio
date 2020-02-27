bdacd4655ddab61e29162cc57241f171c0589018
Kathleen Trainor <kathleentrainor9@gmail.com>
2020-02-26 19:33:04 -0500

    Fixes typo in db connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 // 'mysql://b0404b70907b7e:f6cb7138@us-cdbr-iron-east-04.cleardb.net/heroku_e319ed1fe746801?reconnect=true'
 
 // connects to stock_portfolio database
-const db = new Sequelize('mysql://b0404b70907b7e:f6cb7138@us-cdbr-iron-east-04.cleardb.net/heroku_e319ed1fe746801?reconnect=true);
+const db = new Sequelize('mysql://b0404b70907b7e:f6cb7138@us-cdbr-iron-east-04.cleardb.net/heroku_e319ed1fe746801?reconnect=true');
 
 db.authenticate()
   .then(() => console.log('Connected to the database'))
